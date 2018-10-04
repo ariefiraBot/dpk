7c3a54247dcf51986fbc000bcb5b7e38c8d1f197
ariefiraBot <42173620+ariefiraBot@users.noreply.github.com>
2018-10-04 11:54:52 +0900

    Update config.py

--- ARIFISTIFIK/config.py
+++ ARIFISTIFIK/config.py
@@ -28,10 +28,10 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '1.4.17'
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[112]
+    APP_VER     = '5.8.0'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'ARIFISTIFIK'
+    SYSTEM_NAME = 'ARIFIRA'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
